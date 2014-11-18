7eec9aa7a3b6f10495d70bbe288069e4746ee956
cynful <cynthia.m.kwok@gmail.com>
2014-11-18 15:34:39 -0800

    update README, problem 5

--- p5.py
+++ p5.py
@@ -8,3 +8,34 @@ Problem 5
 What is the smallest positive number that is evenly divisible by all the numbers from 1 to 20?
 """
 #!/usr/bin/python
+
+def primeFactors(n):
+    primefactors = []
+    i = 2
+    while i*i<=n:
+        if n%i:
+            i += 1
+        else:
+            n //= i
+            primefactors.append(i)
+    if n>1:
+        primefactors.append(n)
+    
+    return primefactors
+
+minfactors = {}
+for i in range(1, 21):
+    primefactors = primeFactors(i)
+    for key in primefactors:
+        val = primefactors.count(key)
+        if minfactors.has_key(key):
+            if minfactors[key] < val:
+                minfactors[key] = val
+        else:
+            minfactors[key] = val
+
+product = 1
+for key, val in minfactors.iteritems():
+    product *= (key**val)
+
+print product 
