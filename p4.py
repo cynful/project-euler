d8056c51e9e05d485c069bfe3d63990643b9491b
cynful <cynthia.m.kwok@gmail.com>
2014-11-18 17:56:52 -0800

    xrange to range

--- p4.py
+++ p4.py
@@ -10,8 +10,8 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 #!/usr/bin/python
 
 palindrome = 0
-for i in xrange(100, 1000):
-    for j in xrange(100, 1000):
+for i in range(100, 1000):
+    for j in range(100, 1000):
         prod = i*j
         s = str(prod)
         if s == s[::-1] and prod > palindrome:
