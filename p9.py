02081e4213b9b7ce417ae1d272a2fff011f8c942
cynful <cynthia.m.kwok@gmail.com>
2014-11-18 17:51:59 -0800

    problem 9

---
+++ p9.py
@@ -0,0 +1,25 @@
+"""
+Special Pythagorean triplet
+Problem 9
+
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for
+which,
+    a^2 + b^2 = c^2
+
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+"""
+#!/usr/bin/python
+
+def pythagoreanTriplet():
+    sum = 1000
+    for a in range(1, sum/3):
+        b = a+1
+        for b in range(1, sum/2):
+            c = sum - a - b
+            if a**2 + b**2 == c**2:
+                return a*b*c
+
+print pythagoreanTriplet()
